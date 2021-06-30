43e3232e0fb1f6b9cd13551abf565ea45a8cf91d
Sam Kosky <benoit.perigaud@aginic.com>
2021-06-30 03:36:07 +0000

    add new models for demo

---
+++ macros/test_is_even.sql
@@ -0,0 +1,21 @@
+{% macro test_is_even(model, column_name) %}
+
+with validation as (
+    select
+        {{ column_name }} as even_field
+    from {{ model }}
+),
+
+validation_errors as (
+    select
+        even_field
+    from validation
+    -- if this is true, then even_field is actually odd!
+    where (even_field % 2) = 1
+)
+
+-- if COUNT returns > 0 then the test has failed
+select count(*)
+from validation_errors
+
+{% endmacro %}
